3353fd52ef023eb5dca48012c4b6a3c7715bf261
Christina_Sereday <christina.sereday@gmail.com>
2023-03-10 14:58:00 -0500

    Added CheatDescription

--- Game_Cheats_App/Models/Cheats.cs
+++ Game_Cheats_App/Models/Cheats.cs
@@ -9,6 +9,8 @@ namespace Game_Cheats_App.Models
         public int CheatId { get; set; }
         public string? CheatName { get; set; }
 
+        public string? CheatDescription { get; set; }
+
         //One cheat can have many games (same cheat can be used in many games w/ different platforms)//might have to remove 
         public List<Games_Cheats>? GamesCheats { get; set; }
 
